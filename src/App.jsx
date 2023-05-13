c4088b7e3022831b174367f8209b01c85fbe312b
Max Kotyk <mxm.kotyk@gmail.com>
2023-05-13 18:06:52 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        backgroundColor: '#282c34',
+        color: 'white',
+      }}
+    >
+      <p>
+        Edit <code>src/App.jsx</code> and save to reload.
+      </p>
+      <a
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          color: '#61dafb',
+        }}
+      >
+        Learn React
+      </a>
+    </div>
+  );
+};
